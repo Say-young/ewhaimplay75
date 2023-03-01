3f5b661f72bfca73e5a6135c4ad77345304fbdba
hmuri <113418916+hmuri@users.noreply.github.com>
2023-03-01 09:55:36 +0900

    3/1 좌석 업데이트

--- thesun/src/components/Ticket/Seats.js
+++ thesun/src/components/Ticket/Seats.js
@@ -136,19 +136,19 @@ function Seats() {
 	const lastSeatList = [
 		{
 			date : '03/09 19:30',
-			numOfSeats : 184
-		},
-		{
-			date : '03/10 19:30',
 			numOfSeats : 180
 		},
 		{
+			date : '03/10 19:30',
+			numOfSeats : 176
+		},
+		{
 			date : '03/11 14:00',
-			numOfSeats : 182
+			numOfSeats : 181
 		},
 		{
 			date : '03/11 19:00',
-			numOfSeats : 185
+			numOfSeats : 186
 		}
 	]
 
